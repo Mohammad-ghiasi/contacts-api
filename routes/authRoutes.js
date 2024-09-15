ce7bdba2469a679c44cf41cb9aae1bd44f564db0
mohammad <mohammadghiasi005@gmail.com>
2024-09-15 18:53:54 +0330

    3y9dqwdcwdc

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -57,14 +57,10 @@ router.post('/login', async (req, res) => {
 
         const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
         res.cookie('auth_token', token, {
-           
+            httpOnly: true,
             maxAge: 3600000, // 1 hour
-           
-        });
-        res.cookie('auth_token', 'mamad', {
-           
-            maxAge: 3600000, // 1 hour
-           
+            secure: false,
+            sameSite: 'Lax',
         });
 
         res.json({ message: 'Login successful', token: token });
