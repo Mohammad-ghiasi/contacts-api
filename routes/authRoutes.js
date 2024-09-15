c82db62f4d2abc25118c8de866a4b810e64e62e0
mohammad <mohammadghiasi005@gmail.com>
2024-09-15 18:24:13 +0330

    moo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -59,6 +59,7 @@ router.post('/login', async (req, res) => {
         res.cookie('auth_token', token, {
             httpOnly: true,
             maxAge: 3600000, // 1 hour
+            sameSite: true,
         });
 
         res.json({ message: 'Login successful', token: token });
